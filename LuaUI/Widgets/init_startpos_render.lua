f90d1a4dc02105e9982102868817578859b488f2
_Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2020-09-07 02:02:06 -0400

    Create lua renderer for startpositions

---
+++ LuaUI/Widgets/init_startpos_render.lua
@@ -0,0 +1,127 @@
+function widget:GetInfo()
+	return {
+		name      = "Lua Start positions",
+		desc      = "Draws lua start positions.",
+		author    = "Shaman",
+		date      = "Aug 21, 2020",
+		license   = "PD",
+		layer     = -1,
+		enabled   = true,
+		alwaysStart = true,
+	}
+end
+
+--Spring.IsAABBInView
+
+--openGL speedups
+local glColor = gl.Color
+local glDepthTest = gl.DepthTest
+local glDepthMask = gl.DepthMask
+local glLighting = gl.Lighting
+local glPushMatrix = gl.PushMatrix
+local glLoadIdentity = gl.LoadIdentity
+local glTranslate = gl.Translate
+local glRotate = gl.Rotate
+local glUnitShape = gl.UnitShape
+local glPopMatrix = gl.PopMatrix
+local glText = gl.Text
+local spEcho = Spring.Echo
+local spGetTeamColor = Spring.GetTeamColor
+local spGetGroundHeight = Spring.GetGroundHeight
+local spGetPlayerInfo = Spring.GetPlayerInfo
+local spWorldToScreenCoords = Spring.WorldToScreenCoords
+local spGetTeamInfo = Spring.GetTeamInfo
+local spSendLuaRulesMsg = Spring.SendLuaRulesMsg
+local knownCommanderStarts = {} -- teamID = {[num] = {x=x,y=y,z=z,def=UnitDefID}} NB: Teams may have multiple commanders. (See: commshare, uneven teams, etc)
+local startPoses = 0
+
+local function DrawCommander(uDefID, teamID, ux, uy, uz, startposnum) -- borrowed this from initial queue.
+	local r,g,b,a = spGetTeamColor(teamID)
+	local textZ = uz - 10
+	local textY = spGetGroundHeight(ux,textZ)
+	local name = spGetPlayerInfo(select())
+	if #spGetPlayerList(teamID) > 1 then
+		name = name .. "'s squad (" .. startposnum .. ")"
+	else
+		name = name .. "(" .. startposnum .. ")"
+	end
+	glColor(1.0, 1.0, 1.0, 1.0)
+	glDepthTest(GL.LEQUAL)
+	glDepthMask(true)
+	glLighting(true)
+	glPushMatrix()
+		glLoadIdentity()
+		glTranslate(ux, uy, uz)
+		glRotate(0, 0, 1, 0)
+		glUnitShape(uDefID, teamID, false, false, false)
+	glPopMatrix()
+	local sx, sy, sz = spWorldToScreenCoords(ux,uy,uz)
+	if sx then
+		glColor(r,g,b,a)
+		glText(name,sx,sz,'co')
+		glColor(1,1,1,1)
+	end
+	glLighting(false)
+	glDepthTest(false)
+	glDepthMask(false)
+end
+
+local function CheckIfExists(teamID,id)
+	local startpos = knownCommanderStarts[teamID]
+	for i=1, #startpos do
+		if startpos[i].id == id then
+			return i
+		end
+	end
+end
+
+local function StartUpdated(unitdef,teamID, x,y,z,commid) -- note playerID can be 
+	if knownCommanderStarts[teamID] == nil then
+		knownCommanderStarts[teamID] = {[1] = {x = x, y = y, z = z, def = unitdef, id = commid}}
+		startPoses = startPoses + 1
+	else
+		local index = CheckIfExists(teamID, commid)
+		if index then
+			knownCommanderStarts[teamID][index].x = x
+			knownCommanderStarts[teamID][index].y = y
+			knownCommanderStarts[teamID][index].z = z
+			knownCommanderStarts[teamID][index].def = unitdef
+		else
+			knownCommanderStarts[teamID][#knownCommanderStarts[teamID]+1] = {x = x, y = y, z = z, def = unitdef, id = commid}
+		end
+	end
+	if WG then
+		WG.StartPositions = knownCommanderStarts -- for other widgets.
+	end
+end
+
+function widget:DrawWorld()
+	if startPoses > 0 then
+		for id,startpos in pairs(knownCommanderStarts) do
+			if #startpos > 0 then
+				for i=1, #startpos do
+					local x,y,z = startpos[i].x, startpos[i].y,startpos[i].z
+					DrawCommander(startpos[i].def,id,x,y,z,i)
+				end
+			end
+		end
+	end
+end
+
+local function Shutdown()
+	widgetHandler:RemoveWidget(widget)
+	WG.StartPositions = nil
+end
+
+function widget:GameStart()
+	Shutdown()
+end
+
+function widget:Initialize()
+	if spGetGameFrame() > 0 then
+		spEcho("game started. stopping startpos render.")
+		Shutdown()
+	end
+	widgetHandler:RegisterGlobal('StartPosUpdate',StartUpdated)
+	spSendLuaRulesMsg("startpos resend") -- tell syncland i crashed or restarted.
+end
