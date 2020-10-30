8cbdd5a17203cc1671edf4391bcd6309cc3294e8
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2020-10-29 21:27:17 -0400

    Fix #4158

--- scripts/hoverdepthcharge.lua
+++ scripts/hoverdepthcharge.lua
@@ -159,8 +159,8 @@ end
 local function FakeWeaponShoot(targetID)
 	local reloaded = select(2, spGetUnitWeaponState(unitID,1))
 	if reloaded then
-		local x,y,z = Spring.GetUnitPosition(unitID)
-		local h = Spring.GetGroundHeight(x,z)
+		local x, y, z = Spring.GetUnitPosition(unitID)
+		local h = Spring.GetGroundHeight(x, z)
 		if h > -5 and not GG.OverkillPrevention_CheckBlock(unitID, targetID, 400, 30) then
 			local gameFrame = spGetGameFrame()
 			local reloadMult = spGetUnitRulesParam(unitID, "totalReloadSpeedChange") or 1.0
@@ -179,6 +179,13 @@ function script.BlockShot(num, targetID)
 		return GG.OverkillPrevention_CheckBlock(unitID, targetID, 500, 60)
 	end
 	if num == 2 then
+		if targetID then
+			local tx, ty, tz = Spring.GetUnitPosition(targetID)
+			local groundHeight = Spring.GetGroundHeight(tx, tz)
+			if ty - groundHeight > 25 then -- target is in the air. This gives it a little leeway for stuff thrown slightly off the ground with impulse, etc.
+				return false
+			end
+		end
 		FakeWeaponShoot(targetID)
 	end
 	return true
